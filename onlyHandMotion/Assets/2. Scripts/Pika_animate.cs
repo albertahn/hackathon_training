ea5196e1498c31b40057ad7c5ca4c2c110751234
albertan3 <alban@visa.com>
2016-09-06 01:41:34 -0700

    first

---
+++ onlyHandMotion/Assets/2. Scripts/Pika_animate.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pika_animate : MonoBehaviour {
+
+
+	private bool toggleHit = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (!toggleHit) {
+			
+			this.transform.Rotate (-1, 0, 0);
+
+		} else {//
+			
+			this.transform.Rotate (1, 0, 0);
+
+		}
+
+
+
+	}
+}
---
+++ onlyHandMotion/Assets/2. Scripts/PokiAR_cam.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PokiAR_cam : MonoBehaviour {
+
+	// Use this for initialization
+
+	public WebCamTexture mCamera = null;
+	public GameObject backgroundPlane;
+
+
+	void Start () {
+
+		backgroundPlane = GameObject.Find ("backgroundPlane");
+
+		mCamera = new WebCamTexture ();
+
+		backgroundPlane.GetComponent<Renderer> ().material.mainTexture = mCamera;
+
+		mCamera.Play ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	
+	}//update
+
+
+}//class
